4dbcadff207ba678dca5a12d972b145d0de929c4
psoldatov242 <psoldatov242@gmail.com>
2025-11-01 11:47:55 +0300

    Поправил ошибку

--- Lab3_inform_SQL/Number_2.sql
+++ Lab3_inform_SQL/Number_2.sql
@@ -1,4 +1,1 @@
-SELECT SUM(sales.quantity_sold * products.price)
-FROM sales 
-JOIN products ON sales.product_id = products.id
-WHERE sales.sale_date BETWEEN '2024-01-15' AND '2024-01-18'
+SELECT * FROM products WHERE quantity < 30;
